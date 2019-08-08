ef154c5226be9d522d89a6dbe70444cc80e89438
beellz <belal20795@gmail.com>
2019-08-08 22:36:14 +0530

    adding creat node element

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
     
 function start(){    
     let add = document.querySelector(".input-add").value;
-    document.querySelector(".here").innerHTML = add;
-    
+    // document.querySelector(".here").innerHTML = add;
+
+
+        
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(add);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("li-on").appendChild(node);    // Append <li> to <ul> with id="myList"
 }
