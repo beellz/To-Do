d678ea2cc2ba4852cfcdc8edad00dcb743d4e48c
beellz <belal20795@gmail.com>
2020-05-23 21:11:12 +0530

    removed unwanted comments

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ for (i = 0; i < close.length; i++) {
 }
 
 
-// function start(){    
-//     let add = document.querySelector(".input-add").value;
-//     // document.querySelector(".here").innerHTML = add;
-//     var node = document.createElement("li");                 // Create a <li> node
-//     var textnode = document.createTextNode(add);         // Create a text node
-//     node.appendChild(textnode);                              // Append the text to <li>
-//     document.getElementById("li-on").appendChild(node);    // Append <li> to <ul> with id="myList"
+
 
 function newElement() {
     var li = document.createElement("li");
